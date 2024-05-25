132f939bfd91ef4654d4b9be82d17a0036dc3187
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-25 17:44:46 +0300

    Update user.builder.ts

--- src/builders/user.builder.ts
+++ src/builders/user.builder.ts
@@ -1,1 +1,38 @@
 import User from "./../database/models/user";
+
+const operationsObj = {
+    "EQ": "",
+    "NE": "",
+    "LT": "",
+    "GT": ""
+}
+
+const convertCondition = () => {
+
+}
+
+class UserBuilder {
+    public limit: number = 20;
+    public offset: number = 0;
+    public fields: any = ["first_name", "last_name"];
+    public condition: any = {
+        type: "AND",
+        items: [
+            {
+                field: "first_name",
+                operation: "EQ",
+                value: "mario"
+            },
+            {
+                field: "last_name",
+                operation: "EQ",
+                value: "nikolov"
+            }
+        ]
+    };
+
+    buildQuery(): any {
+        let queryObj: any = { limit: this.limit, offset: this.offset, attributes: [...this.fields] };
+
+    }
+}
