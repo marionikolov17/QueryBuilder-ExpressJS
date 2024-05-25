e853a9cf10163f5df08687e5b56f244974aaa599
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-25 16:41:35 +0300

    Create db.config.ts

---
+++ src/config/db.config.ts
@@ -0,0 +1,9 @@
+export const DB_USERNAME = "root";
+
+export const DB_PASSWORD = "admin56Mn7=33Zz";
+
+export const DB_NAME = "athleans-app";
+
+export const DB_HOST = "localhost";
+
+export const DB_PORT = 3306;
