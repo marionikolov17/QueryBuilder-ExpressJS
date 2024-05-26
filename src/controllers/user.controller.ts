fcae83861f5a37ce626d4066c0cdbe09e5565097
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-26 20:44:14 +0300

    Update user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -15,6 +15,17 @@ router.get("/getList", async (req: express.Request, res: express.Response) => {
   });
 });
 
+router.get("/getDetail", async (req: express.Request, res: express.Response) => {
+  const user = await userService.getUser(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user
+    },
+  });
+});
+
 router.post("/create", async (req: express.Request, res: express.Response) => {
   try {
     const createdUser = await userService.createUser(req.body);
@@ -34,17 +45,6 @@ router.post("/create", async (req: express.Request, res: express.Response) => {
       },
     });
   }
-});
-
-router.get("/getDetail", async (req: express.Request, res: express.Response) => {
-  const user = await userService.getUser(req.body);
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      user
-    },
-  });
 });
 
 export default router;
