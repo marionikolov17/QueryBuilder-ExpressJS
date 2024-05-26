698a1ba18e5dc58e4fe98cef765dda4a0f192e67
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-26 12:34:15 +0300

    Create convertCondition in user builder

--- src/builders/user.builder.ts
+++ src/builders/user.builder.ts
@@ -1,77 +1,115 @@
 import { Op } from "sequelize";
 import User from "./../database/models/user";
 
-const operationsObj = {
-    "EQ": Op.eq,
-    "NE": Op.ne,
-    "LT": Op.lt,
-    "GT": Op.gt
-}
+const operationsObj: any = {
+  AND: Op.and,
+  OR: Op.or,
+  EQ: Op.eq,
+  NE: Op.ne,
+  LT: Op.lt,
+  GT: Op.gt,
+};
 
 const fieldMapObj: any = {
-    "uid": "id",
-    "firstName": "first_name",
-    "lastName": "last_name",
-    "userName": "username",
-    "email": "email",
-    "password": "password",
-    "profilePicture": "profile_picture_url",
-    "country": "country",
-    "userLanguages": "languages",
-    "phoneNumber": "phone_number",
-    "userRole": "user_role",
-    "visible": "visible",
-    "dateCreated": "date_created",
-}
+  uid: "id",
+  firstName: "first_name",
+  lastName: "last_name",
+  userName: "username",
+  email: "email",
+  password: "password",
+  profilePicture: "profile_picture_url",
+  country: "country",
+  userLanguages: "languages",
+  phoneNumber: "phone_number",
+  userRole: "user_role",
+  visible: "visible",
+  dateCreated: "date_created",
+};
 
 const convertFields = (fields: any) => {
-    let resultFields = [];
-    for (let [key, value] of Object.entries(fields)) {
-        if (!fieldMapObj.hasOwnProperty(key)) {
-            // Field from another table
-            continue
-        }
-
-        resultFields.push(fieldMapObj[key]);
+  let resultFields = [];
+  for (let [key, value] of Object.entries(fields)) {
+    if (!fieldMapObj.hasOwnProperty(key)) {
+      // Field from another table
+      continue;
     }
 
-    return resultFields;
-}
+    resultFields.push(fieldMapObj[key]);
+  }
 
-const convertCondition = () => {
+  return resultFields;
+};
 
-}
+const convertCondition = (condition: any) => {
+  let resultConditions: any;
+
+  if (condition.type === "AND") {
+    resultConditions = [];
+
+    for (let item of condition.items) {
+      let logicalObj: any = {};
+      logicalObj[item.field] = {
+        [operationsObj[item.operation]]: [item.value],
+      };
+      resultConditions.push(logicalObj);
+    }
+
+    return resultConditions;
+  }
+
+  resultConditions = {
+    [operationsObj[condition.type]]: [],
+  };
+  let logicalArr = [];
+
+  for (let item of condition.items) {
+    let logicalObj: any = {};
+    logicalObj[item.field] = {
+      [operationsObj[item.operation]]: [item.value],
+    };
+    logicalArr.push(logicalObj);
+  }
+
+  resultConditions[operationsObj[condition.type]] = logicalArr;
+  return resultConditions;
+};
 
 class UserBuilder {
-    public limit: number = 20;
-    public offset: number = 0;
-    public fields: any = {
-        "uid": 1,
-        "firstName": 1,
-        "lastName": 1
-    };
-    public condition: any = {
-        type: "AND",
-        items: [
-            {
-                field: "first_name",
-                operation: "EQ",
-                value: "mario"
-            },
-            {
-                field: "last_name",
-                operation: "EQ",
-                value: "nikolov"
-            }
-        ]
-    };
+  public limit: number = 20;
+  public offset: number = 0;
+  public fields: any = {
+    uid: 1,
+    firstName: 1,
+    lastName: 1,
+  };
+  public condition: any = {
+    type: "OR",
+    items: [
+      {
+        field: "first_name",
+        operation: "NE",
+        value: "mario",
+      },
+      {
+        field: "first_name",
+        operation: "EQ",
+        value: "ivan",
+      },
+    ],
+  };
 
-    public buildQuery(): any {
-        let queryObj: any = { limit: this.limit, offset: this.offset, attributes: [], where: {} };
-        queryObj["attributes"] = convertFields(this.fields);
-
-        return User.findAll(queryObj);
-    }
+  public buildQuery(): any {
+    let queryObj: any = {
+      limit: this.limit,
+      offset: this.offset,
+      attributes: [],
+      where: {},
+    };
+    queryObj["attributes"] = convertFields(this.fields);
+    console.log(convertCondition(this.condition));
+    queryObj["where"] = convertCondition(this.condition);
+    return User.findAll(queryObj);
+  }
 }
 
 export default UserBuilder;
