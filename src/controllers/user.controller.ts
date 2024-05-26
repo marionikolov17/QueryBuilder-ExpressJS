ac4b3d09b8cd2c54ca5436f9ccb060f1aba06e34
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-26 18:34:57 +0300

    Update user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ import * as userService from "./../services/user.service";
 
 const router = express.Router();
 
-router.get("/", async (req: express.Request, res: express.Response) => {
+router.get("/getList", async (req: express.Request, res: express.Response) => {
   const users = await userService.getUsers(req.body);
 
   res.status(200).json({
