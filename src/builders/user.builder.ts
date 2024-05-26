7f8c63c8f280adfc2f40567072bfa318b7905a89
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-26 13:05:26 +0300

    Update user.builder.ts

--- src/builders/user.builder.ts
+++ src/builders/user.builder.ts
@@ -1,23 +1,22 @@
 import User from "./../database/models/user";
 import { convertFields, convertCondition } from "./converter";
 
-const fieldMapObj: any = {
-  uid: "id",
-  firstName: "first_name",
-  lastName: "last_name",
-  userName: "username",
-  email: "email",
-  password: "password",
-  profilePicture: "profile_picture_url",
-  country: "country",
-  userLanguages: "languages",
-  phoneNumber: "phone_number",
-  userRole: "user_role",
-  visible: "visible",
-  dateCreated: "date_created",
-};
-
 class UserBuilder {
+  private fieldMapObj: any = {
+    uid: "id",
+    firstName: "first_name",
+    lastName: "last_name",
+    userName: "username",
+    email: "email",
+    password: "password",
+    profilePicture: "profile_picture_url",
+    country: "country",
+    userLanguages: "languages",
+    phoneNumber: "phone_number",
+    userRole: "user_role",
+    visible: "visible",
+    dateCreated: "date_created",
+  };
   public limit: number = 20;
   public offset: number = 0;
   public fields: any = {
@@ -48,7 +47,7 @@ class UserBuilder {
       attributes: [],
       where: {},
     };
-    queryObj["attributes"] = convertFields(this.fields, fieldMapObj);
+    queryObj["attributes"] = convertFields(this.fields, this.fieldMapObj);
     console.log(convertCondition(this.condition));
     queryObj["where"] = convertCondition(this.condition);
     return User.findAll(queryObj);
