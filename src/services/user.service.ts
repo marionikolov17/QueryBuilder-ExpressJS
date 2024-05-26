b5c31d84c8de7a9417eb4af1c788684cd935857c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-26 12:51:34 +0300

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,15 +1,13 @@
-
-
+import UserBuilder from "./../builders/user.builder";
+import User from "./../database/models/user";
 
 
 export const getUsers = async () => {
+    let builder = new UserBuilder;
 
+    return builder.buildQuery();
 }
 
-export const getUser = async (id: any) => {
+export const getUser = async (id: any) => {}
 
-}
-
-export const createUser = async (data: any) => {
-    
-}
+export const createUser = async (data: any) => User.create(data);
