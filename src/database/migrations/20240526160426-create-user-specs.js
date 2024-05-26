2f1c8c653d7adf673f9fe69f8eaa728a38b2c575
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-26 19:04:38 +0300

    Update

--- src/database/migrations/20240526160426-create-user-specs.js
+++ src/database/migrations/20240526160426-create-user-specs.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UserSpecs', {
+    await queryInterface.createTable('user_specs', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -23,6 +23,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UserSpecs');
+    await queryInterface.dropTable('user_specs');
   }
 };
--- src/database/models/user_specs.js
+++ src/database/models/user_specs.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class UserSpecs extends Model {
+  class user_specs extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,11 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  UserSpecs.init({
+  user_specs.init({
     name: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'UserSpecs',
+    modelName: 'user_specs',
   });
-  return UserSpecs;
+  return user_specs;
 };
