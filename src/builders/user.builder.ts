73aa844006720bab250320b1ec6d9396b2dcb838
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-28 23:55:37 +0300

    Update user.builder.ts

--- src/builders/user.builder.ts
+++ src/builders/user.builder.ts
@@ -1,7 +1,6 @@
-import { Op } from "sequelize";
-import User from "./../database/models/user";
 import { convertFields, convertCondition, findAssociatedFiels, convertAssociatedFields } from "./converter";
 import {fieldsMap} from "./fields";
+import connection from "./../database/connection";
 
 class UserBuilder {
   private fieldMapObj: any = fieldsMap["user"];
@@ -15,7 +14,7 @@ class UserBuilder {
   public condition: any; // You can pass default conditions here
   public id: any = null;
 
-  public buildQuery(): any {
+  public async buildQuery(): Promise<any> {
     let queryObj: any = {
       limit: this.limit,
       offset: this.offset,
@@ -23,7 +22,7 @@ class UserBuilder {
       where: {},
       include: []
     };
-    console.log(User.associations.user_specs)
+
     if(this.id !== null) {
       delete queryObj["limit"];
       delete queryObj["offset"];
@@ -35,25 +34,16 @@ class UserBuilder {
     let associatedFields = findAssociatedFiels(this.fields, this.fieldMapObj);
     queryObj["include"] = convertAssociatedFields(associatedFields, "user");
     
-    return User.findAll(queryObj);
-    /* return User.findAll({
-      limit: 20,
-      offset: 0,
-      attributes: ["first_name", "last_name", "id"],
-      where: {
-        [Op.or]: [{ first_name: "mario" }]
-      },
-      include: [
-        {
-          association: "user_specs",
-          attributes: ["sex"],
-          where: {
-            sex: "male"
-          }
-        }
-      ]
-    }) */
+    //return User.findAll(queryObj);
+
+    return connection.query(`
+      SELECT first_name, last_name, users.id FROM users LEFT JOIN user_specs ON users.id = user_specs.id WHERE user_specs.sex = 'female' OR users.first_name = 'mario'
+    `)
   }
 }
+
+/* 
+SELECT * FROM query_builder.users LEFT JOIN query_builder.user_specs ON query_builder.users.id = query_builder.user_specs.user_id WHERE query_builder.user_specs.sex = 'female' OR query_builder.users.first_name = 'mario';
+*/
 
 export default UserBuilder;
