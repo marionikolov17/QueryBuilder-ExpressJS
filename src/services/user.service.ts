679454e159a8fd8645e3d7c8f954f56cbb16d685
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-29 19:05:51 +0300

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,23 +1,7 @@
-import UserBuilder from "./../builders/user.builder";
-import User from "./../database/models/user";
-import UserSpecs from "./../database/models/user_specs";
+export const getUsers = async (payload: any) => {};
 
-export const getUsers = async (payload: any) => {
-    let builder = new UserBuilder;
-    builder.fields = payload.what;
-    builder.condition = payload.condition;
+export const getUser = async (payload: any) => {};
 
-    return builder.buildQuery();
-}
+export const createUser = async (data: any) => {};
 
-export const getUser = async (payload: any) => {
-    let builder = new UserBuilder;
-    builder.id = payload.id;
-    builder.fields = payload.what;
-
-    return builder.buildQuery();
-}
-
-export const createUser = async (data: any) => User.create(data);
-
-export const createUserSpecs = async (data: any) => UserSpecs.create(data);
+export const createUserSpecs = async (data: any) => {};
