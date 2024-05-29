5e930100c7efb07e3e2e0b45be56a55dc0a3a618
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-29 19:05:06 +0300

    Update 20240529160419_user_specs.ts

--- src/database/migrations/20240529160419_user_specs.ts
+++ src/database/migrations/20240529160419_user_specs.ts
@@ -1,10 +1,12 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("user_specs", (table) => {
+
+    });
 }
 
-
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists("user_specs");
 }
 
