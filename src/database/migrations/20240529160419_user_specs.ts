6b832fa87586fab9770d7e40a50158d8ee478187
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-29 19:26:42 +0300

    Update 20240529160419_user_specs.ts

--- src/database/migrations/20240529160419_user_specs.ts
+++ src/database/migrations/20240529160419_user_specs.ts
@@ -3,7 +3,8 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable("user_specs", (table) => {
         table.increments("id").primary().unsigned().unique();
-        table.integer("user_id").notNullable().unique().references("id").inTable("users");
+        table.integer("user_id").unsigned()
+        table.foreign("user_id").references("users.id");
         table.tinyint("weight").unsigned().notNullable().defaultTo(0);
         table.tinyint("weight_goal").unsigned().notNullable().defaultTo(0);
         table.tinyint("height").unsigned().notNullable().defaultTo(0);
