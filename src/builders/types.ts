116f603f858029103a11ed622c08f7bd805b13fc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-30 21:53:19 +0300

    Update types.ts

--- src/builders/types.ts
+++ src/builders/types.ts
@@ -1,17 +1,17 @@
 export interface QueryParams {
-    limit: number | null | undefined;
-    offset: number | null | undefined;
-    what: Record<string, number> | null | undefined;
-    condition: Condition | null | undefined
+  limit: number | null | undefined;
+  offset: number | null | undefined;
+  what: Record<string, number> | null | undefined;
+  condition: Condition | null | undefined;
 }
 
 export interface Condition {
-    type: string;
-    items: Array<ConditionItem>
+  type: string;
+  items: Array<ConditionItem>;
 }
 
 export interface ConditionItem {
-    field: string;
-    operation: string;
-    value: any;
+  field: string;
+  operation: string;
+  value: any;
 }
